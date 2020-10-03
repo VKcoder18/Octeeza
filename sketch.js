124a6cf318b1d587fa96934fe3c2a1ec9ab63078
VKcoder18 <67476434+VKcoder18@users.noreply.github.com>
2020-10-03 00:50:16 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+
+
+function draw() {
+  Text("Step 1: get a slice fo bread and but some ketchup on it then spread the ketchup, After put cheese on it and fry it. EAT IT")
+
+
+  drawSprites();
+ 
+}
+
+
+
